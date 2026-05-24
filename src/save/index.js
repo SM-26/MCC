4ff8ef5c0712cd52041c545199ba8c0081d201b0
Natalie <287457272+Natalie-paia@users.noreply.github.com>
2026-05-25 00:11:53 +0200

    Hard reset: kept only essential files

--- src/save/index.js
+++
@@ -1,2 +0,0 @@
-// Save module - ES Module exports for browser use
-export { loadSave, saveGame, createDefaultSave, createValidatedSave } from './save.js';
--- src/world/index.js
+++
@@ -1,3 +0,0 @@
-// World module - ES Module exports for browser use
-export { generateInitialWorldGrid } from './grid.js';
-export { performExploration } from './discovery.js';
